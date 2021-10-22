91671dfd38d594d3631e88b0215a3b9a4c0baacb
Vera Vlasova <vlasovavery@gmail.com>
2021-10-22 22:00:20 +0500

    rewrite tests to asserts

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,16 @@
+import {strict as assert} from 'assert';
 import capitalize from "../src/capitalize.js";
 
-if (capitalize('hello') !== 'Hello') {
- throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize(''), 'Hello');
+assert.strictEqual(capitalize('hello'), ' ');
 
-if (capitalize('') !== '') {
- throw new Error('Функция работает неверно!');
-}
+//Ручные утверждения
+// if (capitalize('hello') !== 'Hello') {
+//  throw new Error('Функция работает неверно!');
+// }
 
-console.log('Все тесты пройдены!');
+// if (capitalize('') !== '') {
+//  throw new Error('Функция работает неверно!');
+// }
+
+// console.log('Все тесты пройдены!');
